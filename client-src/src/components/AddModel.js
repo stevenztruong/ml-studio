3cbc9c0bc165d3589897ceba4920cb23637491f7
Farokh Confectioner <lyghtsped@gmail.com>
2021-05-21 15:21:06 -0700

    Add basic addmodel page ui

--- client-src/src/components/AddModel.js
+++ client-src/src/components/AddModel.js
@@ -5,8 +5,12 @@ import {
   FormLabel,
   RadioGroup,
   Radio,
-  Card
+  Card,
+  Button,
+  TextField
 } from '@material-ui/core';
+
+import NavBar from './NavBar';
 
 export default class AddModel extends React.Component {
   constructor(props) {
@@ -23,37 +27,63 @@ export default class AddModel extends React.Component {
     this.setState({ selectedModel: e.target.value });
   }
 
+  handleCreateModel = () => {
+
+  }
+
+  handle
+
+  renderModelParameters = () => {
+    if (this.state.selectedModel === 'knn') {
+      return (
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Model Parameters</FormLabel>
+          <RadioGroup aria-label="parameters" name="parameters" value={this.state.selectedModel} onChange={this.handleSelectedModelChange}>
+            <FormControlLabel labelPlacement='start' label="Layers: " control={<TextField style={{ marginLeft: '10px' }} value={this.state.knnLayers} onChange={(e) => this.setState({ knnLayers: e.target.value })} />} />
+          </RadioGroup>
+        </FormControl>
+      )
+    }
+  }
+
   render() {
     return (
-      <div style={{ display: 'flex', height: '100%'}}>
-        <div style={{ width: '33%', height: '100%' }}>
-          <Card style={{ height: '50%' }}>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Select a model:</FormLabel>
-              <RadioGroup aria-label="model" name="model" value={this.state.selectedModel} onChange={this.handleSelectedModelChange}>
-                <FormControlLabel value="knn" control={<Radio />} label="KNN" />
-                <FormControlLabel value="svm" control={<Radio />} label="SVM" disabled />
-              </RadioGroup>
-            </FormControl>
-          </Card>
-          <Card style={{ height: '50%' }}>
-            <div>
-              params go here
-            </div>
-          </Card>
-        </div>
-
-        <div style={{ width: '33%' }}>
-          <div>
-            Upload training data here
-        </div>
-          <div>
-            upload testing data here
+      <div>
+        <NavBar />
+        <div style={{ display: 'flex', height: '100%' }}>
+          <div style={{ width: '33%', height: '100%' }}>
+            <Card style={{ height: '50%' }}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Select a model:</FormLabel>
+                <RadioGroup aria-label="model" name="model" value={this.state.selectedModel} onChange={this.handleSelectedModelChange}>
+                  <FormControlLabel value="knn" control={<Radio />} label="KNN" />
+                  <FormControlLabel value="svm" control={<Radio />} label="SVM" disabled />
+                </RadioGroup>
+              </FormControl>
+            </Card>
+            <Card style={{ height: '50%' }}>
+              {this.renderModelParameters()}
+            </Card>
           </div>
-        </div>
 
-        <div style={{ width: '33%' }}>
-          Create model button goes here
+          <div style={{ width: '33%' }}>
+            <div style={{ padding: "10px" }}>
+              Upload training data:
+              <input type="file"
+                id="uploadTrainingData"
+                accept="file/json" onChange={this.updateTrainingData} required />
+            </div>
+            <div style={{ padding: "10px" }}>
+              Upload testing data:
+              <input type="file"
+                id="uploadTestingData"
+                accept="file/json" onChange={this.updateTestingData} required />
+            </div>
+          </div>
+
+          <div style={{ width: '33%' }}>
+            <Button onClick={this.handleCreateModel}>Create Model</Button>
+          </div>
         </div>
       </div>
     );
