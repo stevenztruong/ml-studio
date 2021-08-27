f8e36de29fc1f8c155b8e9027d302befdaf91558
Hoang Luong <hoang.luong@sjsu.edu>
2021-08-27 16:30:43 -0700

    switch Aurora DB to MYSQL

--- server-src/utils/dbUtil.js
+++ server-src/utils/dbUtil.js
@@ -1,6 +1,6 @@
 var mysql      = require('mysql');
 exports.connection = mysql.createConnection({
-  host     : 'mlstudio-database-instance-1.c11aba7kcep9.us-west-2.rds.amazonaws.com',
+  host     : 'mlstudiodb.ckvefk2xly8m.us-west-1.rds.amazonaws.com',
   user     : 'mlstudioadmin',
   password : 'dbformlstudio',
   database : 'MLStudio'
