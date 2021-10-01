1f3d623788af638a2ea205fc6790e0ce412df319
Phat Truong <truongvp@us.ibm.com>
2021-10-01 16:34:42 -0700

    Added login page and modified Create account page

--- client-src/src/App.js
+++ client-src/src/App.js
@@ -4,6 +4,7 @@ import ViewModels from './components/ViewModels';
 import AddModel from './components/AddModel';
 import CreateAccount from './components/CreateAccount';
 import Model from './components/Model';
+import Login from './components/Login';
 
 require('dotenv').config()
 
@@ -11,7 +12,8 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route exact path="/" component={ViewModels}></Route>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/viewmodel" component={ViewModels}></Route>
         <Route exact path="/addmodel" component={AddModel}></Route>
         <Route exact path="/model/:id" component={Model}></Route>
         <Route exact path="/createaccount" component={CreateAccount}></Route>
---
+++ client-src/src/components/Login.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Backdrop,
+  CircularProgress,
+  Card,
+  Button,
+  TextField,
+  Grid
+} from '@material-ui/core';
+
+import NavBar from './NavBar';
+import axios from 'axios';
+
+export default class CreateAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      showLoading: false,
+    };
+  }
+
+  componentDidMount = async () => {
+  }
+
+  onLogin = async () => {
+    this.setState({ showLoading: true });
+    await axios.get(
+      process.env.REACT_APP_BACKEND_API_URL + '/v1/login',
+      {
+        headers: {
+          'username': this.state.username,
+          'password': this.state.password,
+        }
+      }
+    ).then(res => {
+      if(res.data.status == "Authenticated"){
+        sessionStorage.setItem('token', res.data.accessToken);
+        this.setState({showLoading: false});
+        window.location = '/viewmodel';
+      }
+      else {
+        alert("Invalid credentials");  
+      }
+    }).catch(error => {
+      this.setState({showLoading: false});
+      alert(error);
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
+            
+          >
+              <h1>Online Machine Learning Studio</h1>
+              <Card style={{ width: '25%', padding: "2%" }}>
+                    <h2 style={{ padding: "10px" }}>Login</h2>
+                    <div style={{ padding: "10px" }}>
+                    <TextField
+                        style={{ width: '80%' }}
+                        id="username"
+                        onChange={(e) => { this.setState({ username: e.target.value }) }}
+                        label="Username"
+                        variant="outlined"
+                        value={this.state.username}
+                    />
+                    </div>
+                    <div style={{ padding: "10px" }}>
+                    <TextField
+                        style={{ width: '80%' }}
+                        id="password"
+                        onChange={(e) => { this.setState({ password: e.target.value }) }}
+                        label="Password"
+                        variant="outlined"
+                        value={this.state.password}
+                        type="password"
+                    />
+                    </div>
+                    <div>
+                    <Button onClick={this.onLogin}>SIGN IN</Button>
+                    </div>
+                    <Button onClick={() => { window.location = '/createaccount' }}>Create an Account</Button>
+                </Card>     
+          </Grid>  
+        </div>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={this.state.showLoading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    );
+  }
+}
