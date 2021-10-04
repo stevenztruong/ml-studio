eb823030c7ea88235e8beb6f632dbcec97643119
Farokh Confectioner <lyghtsped@gmail.com>
2021-10-04 16:38:03 -0700

    Add testing to ML models

--- server-src/ml_invocation/models/decisionTree.py
+++ server-src/ml_invocation/models/decisionTree.py
@@ -6,11 +6,13 @@ import command_types
 import utilities
 import s3Util
 
-def handleDecisionTree(command, trainingData, classificationData, modelName, parameters):
+def handleDecisionTree(command, inputData, classificationData, modelName, parameters):
     if(command == command_types.CREATE_MODEL):
-        return createModel(trainingData, classificationData, modelName, parameters)
+        return createModel(inputData, classificationData, modelName, parameters)
     elif(command == command_types.TRAIN_MODEL):
-        return trainModel(trainingData, classificationData, modelName, parameters)
+        return trainModel(inputData, classificationData, modelName, parameters)
+    elif(command == command_types.TEST_MODEL):
+        return trainModel(inputData, classificationData, modelName, parameters)
     else:
         return(json.dumps({'result': 'error', 'message': "Invalid command. Please use a valid command type."}))
 
@@ -60,6 +62,33 @@ def trainModel(trainingData, classificationData, modelName, parameters):
         print("failure")
         return(json.dumps({'result': 'error', 'message': str(e)}))
 
+def testModel(testingData, classificationData, modelName, parameters):
+    try:
+        classifer = utilities.getModel(modelName)
+        print("Fetched stored Decision Tree classifier")
+        # The commented lines for trainingDataSet and classificationDataSet
+        # are used for retrieving the files locally for TESTING purposes only
+
+        # trainingDataSet = utilities.getFileContents(trainingData)
+        # classificationDataSet = utilities.getFileContents(classificationData)
+
+        print("Fetching training and classification data")
+        testingDataSet = json.loads(s3Util.getFile(testingData))
+        classificationDataSet = json.loads(s3Util.getFile(classificationData))
+        print("Testing Decision Tree model")
+        res = classifer.score(testingDataSet, classificationDataSet)
+
+        print("Score: " + str(res))
+        print("success")
+
+        returnObject = json.dumps({'result': 'success', 'score': str(res)})
+        print("Return object: " + str(returnObject))
+        return(returnObject)
+    except Exception as e:
+        print("failure")
+        return(json.dumps({'result': 'error', 'message': str(e)}))
+
 if __name__ == '__main__':
     createModel("testTrainingData.json", "testClassificationData.json", "testModelName", None)
     trainModel("testTrainingData.json", "testClassificationData.json", "testModelName", None)
+    testModel("testTrainingData.json", "testClassificationData.json", "testModelName", None)
--- server-src/ml_invocation/models/naiveBayes.py
+++ server-src/ml_invocation/models/naiveBayes.py
@@ -10,11 +10,13 @@ import os
 sys.path.append(os.getcwd()+'/server-src/ml_invocation')
 import models_types
 
-def handlNaiveBayes(command, trainingData, classificationData, modelName, parameters, modelType):
+def handlNaiveBayes(command, inputData, classificationData, modelName, parameters, modelType):
     if(command == command_types.CREATE_MODEL):
-        return createModel(trainingData, classificationData, modelName, parameters, modelType)
+        return createModel(inputData, classificationData, modelName, parameters, modelType)
     elif(command == command_types.TRAIN_MODEL):
-        return trainModel(trainingData, classificationData, modelName, parameters)
+        return trainModel(inputData, classificationData, modelName, parameters)
+    elif(command == command_types.TEST_MODEL):
+        return trainModel(inputData, classificationData, modelName, parameters)
     else:
         return(json.dumps({'result': 'error', 'message': "Invalid command. Please use a valid command type."}))
 
@@ -73,6 +75,31 @@ def trainModel(trainingData, classificationData, modelName, parameters):
         print("failure")
         return(json.dumps({'result': 'error', 'message': str(e)}))
 
+def testModel(testingData, classificationData, modelName, parameters):
+    try:
+        classifer = utilities.getModel(modelName)
+        print("Fetched stored Naive Bayes classifier")
+        # The commented lines for trainingDataSet and classificationDataSet
+        # are used for retrieving the files locally for TESTING purposes only
+
+        # trainingDataSet = utilities.getFileContents(trainingData)
+        # classificationDataSet = utilities.getFileContents(classificationData)
+
+        print("Fetching training and classification data")
+        testingDataSet = json.loads(s3Util.getFile(testingData))
+        classificationDataSet = json.loads(s3Util.getFile(classificationData))
+        print("Testing Naive Bayes model")
+        res = classifer.score(testingDataSet, classificationDataSet)
+
+        print("Score: " + str(res))
+        print("success")
+
+        returnObject = json.dumps({'result': 'success', 'score': str(res)})
+        print("Return object: " + str(returnObject))
+        return(returnObject)
+    except Exception as e:
+        print("failure")
+        return(json.dumps({'result': 'error', 'message': str(e)}))
 
 if __name__ == '__main__':
     createModel("testTrainingData.json", "testClassificationData.json",
@@ -83,3 +110,5 @@ if __name__ == '__main__':
                "testClassificationData.json", "testModelNameGaussian", None)
     trainModel("testTrainingData.json",
                "testClassificationData.json", "testModelNameMultinomial", None)
+    testModel("testTrainingData.json", "testClassificationData.json", "testModelNameGaussian", None)
+    testModel("testTrainingData.json", "testClassificationData.json", "testModelNameMultinomial", None)
